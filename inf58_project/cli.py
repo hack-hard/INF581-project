a756953ecde322d38bff5199c346f45e2f5ba4c5
michael.chen <michael.chen@polytechnique.edu>
2024-03-03 17:41:14 +0100

    a 1st environment that is displayed and that plays randomly

--- inf58_project/cli.py
+++ inf58_project/cli.py
@@ -7,7 +7,9 @@ Be creative! do whatever you want!
 - Start a web application
 - Import things from your .base module
 """
-
+import gymnasium
+import numpy
+import time
 
 def main():  # pragma: no cover
     """
@@ -25,4 +27,27 @@ def main():  # pragma: no cover
         * List all available tasks
         * Run an application (Flask, FastAPI, Django, etc.)
     """
-    print("This will do something")
+    env = gymnasium.make(
+        id="ALE/Pacman-v5",
+        full_action_space=False, # action space is Discrete(5) for NOOP, UP, RIGHT, LEFT, DOWN 
+        render_mode="human",
+        obs_type="ram", # observation_space=Box(0, 255, (128,), np.uint8)
+        mode=0, # values in [0,...,7]
+        difficulty=0 # values in [0,1]
+    )
+    
+    for game in range(1): 
+        obs = env.reset()
+        done = False
+        while not done: 
+            env.render()
+            obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
+            done = terminated or truncated
+            time.sleep(1./30)
+            print(reward)
+    
+    env.close()
+    print("End of execution")
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
