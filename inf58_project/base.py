eae101b57d679ecc8e5d4229da81549cc6fa719c
tristan <tristan.hacquard@polytechnique.org>
2024-03-04 22:15:33 +0100

    added temporary model

--- inf58_project/base.py
+++ inf58_project/base.py
@@ -1,3 +1,5 @@
+import torch
+from torch.nn import ReLU
 """
 inf58_project base module.
 
@@ -12,6 +14,31 @@ If you want to replace this with a Flask application run:
 
 and then choose `flask` as template.
 """
+class CuriosityAgent(torch.nn.Module):
+    def __init__(self, channels, state_dim, action_dim):
+        super(CuriosityAgent, self).__init__()
+        self.vision = torch.nn.Sequential(
+            torch.nn.Linear(state_dim,channels[0]),
+            torch.nn.ReLU(),
+            torch.nn.Linear(channels[1],channels[2]),
+            torch.nn.ReLU(),
+            )
+        self.q_value = torch.nn.Sequential(
+            torch.nn.Linear(channels[3],channels[4]),
+            torch.nn.ReLU(),
+            torch.nn.Linear(channels[5],action_dim),
+            torch.nn.ReLU(),
+        )
+        self.curiosity = torch.nn.Sequential(
+            torch.nn.Linear(channels[3] + action_dim,channels[6]),
+            torch.nn.ReLU(),
+            torch.nn.Linear(channels[6],state_dim),
+            torch.nn.ReLU(),
+        )
 
-# example constant variable
-NAME = "inf58_project"
+    def forward(self, x):
+        feature = self.vision(x)
+        values = self.q_value(feature)
+        for i in range(len(self.action_dim)):
+            values += self.norm(feature,self.curiosity(torch.cat((feature,embedding(i,self.action_dim)))
+        return x
