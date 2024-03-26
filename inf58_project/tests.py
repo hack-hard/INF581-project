2b0662d34d10b6757e1a9cbbf673226727b8c7ea
michael.chen <michael.chen@polytechnique.edu>
2024-03-26 15:50:19 +0100

    PacManEnv with 4 ghosts in grayscale + few frames skipped

--- inf58_project/tests.py
+++ inf58_project/tests.py
@@ -3,31 +3,29 @@ import time
 import copy
 import numpy as np
 from inf58_project.pacman_env import PacManEnv
+import cv2
+import sys
 
 def main():
     env = PacManEnv()
     # env.metadata["render_fps"] = 20
 
-    print(env.observation_space.sample()[0])
+    print(env.observation_space.sample().shape)
     for game in range(1):
         obs, info = env.reset()
         done = False
-        for frame in range(200):
+        frame = 0
+        while not done:
             prev_obs = copy.deepcopy(obs)
             env.render()
             sampled_action = env.action_space.sample()
             obs, reward, terminated, truncated, info = env.step(sampled_action)
             print(frame, sampled_action, reward, info)
-            for i in range(128):
-                if i == 0 or i == 100: continue
-                a = prev_obs[i]
-                b = obs[i]
-                if a!=b:
-                    print(i, (a,b))
-            # time.sleep(1)
+            cv2.imwrite("render_result.jpg", obs)
             input()
             done = terminated or truncated
             if done: break
+            frame+=1
 
     env.close()
     print("End of execution")
